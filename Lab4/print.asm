f33e05cfb71d761d730c571226748bf23ffae98b
YuriySavchenko <yurec.proger@gmail.com>
2018-03-25 16:56:23 +0300

    add file with macros wich allow print

---
+++ Lab4/print.asm
@@ -0,0 +1,13 @@
+section .code
+
+%macro print 2
+    mov eax, 4      ; sys_write
+    mov ebx, 1      ; std_out
+    mov ecx, %1     ; mov string for print
+    mov edx, %2     ; mov length of string
+    
+    int 0x80
+
+%endmacro
+
+
