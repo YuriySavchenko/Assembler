2fa8b2381bad119d90ac0b4c550059bdb2d0857f
YuriySavchenko <yurec.proger@gmail.com>
2018-03-25 21:41:47 +0300

    module for addition and subtruction in increased bit rate

--- Lab4/longop.asm
+++ Lab4/longop.asm
@@ -2,20 +2,20 @@ section .code
     
 ; procedure for adding number increased bit rate
 
-Add_618_LONGOP:
+Add_608_LONGOP:
     push ebp
     mov ebp, esp
     
     mov esi, [ebp+16]                     ; ESI - adress of A
     mov ebx, [ebp+12]                     ; EBX - adress of B
-    mov edi, [ebp+19]                     ; EDI - adress of Result
+    mov edi, [ebp+8]                      ; EDI - adress of Result
     
     mov ecx, 19                           ; count of repeat
     mov edx, 0                            ; number of 32-bit group 
 
     clc                                   ; set null bit CF for register EFLAGS
     
-    cycle:
+    .cycle:
     
     mov eax, dword [esi+4*edx]            ; write A to register EAX
     adc eax, dword [ebx+4*edx]            ; adding group of 32-bits
@@ -24,7 +24,7 @@ Add_618_LONGOP:
     inc edx                               ; set counter for number of 32-bit group more on 1 point
     dec ecx                               ; set counter less on 1 point
 
-    jnz cycle                             ; while value in ECX not equal 0 we are repeat cycle
+    jnz .cycle                             ; while value in ECX not equal 0 we are repeat cycle
 
     pop ebp                               ; restoration stack
     ret 12                                ; exit of procedure
@@ -38,14 +38,14 @@ Sub_512_LONGOP:
     
     mov esi, [ebp+16]                     ; ESI - adress of A
     mov ebx, [ebp+12]                     ; EBX - adress of B
-    mov edi, [ebp+19]                     ; EDI - adress of Result
+    mov edi, [ebp+8]                     ; EDI - adress of Result
     
     mov ecx, 16                           ; count of repeat
     mov edx, 0                            ; number of 32-bit group 
 
     clc                                   ; set null bit CF for register EFLAGS
     
-    cycle:
+    .cycle:
     
     mov eax, dword [esi+4*edx]            ; write A to register EAX
     sbb eax, dword [ebx+4*edx]            ; subtruction group of 32-bits
@@ -54,7 +54,7 @@ Sub_512_LONGOP:
     inc edx                               ; set counter for number of 32-bit group more on 1 point
     dec ecx                               ; set counter less on 1 point
 
-    jnz cycle                             ; while value in ECX not equal 0 we are repeat cycle
+    jnz .cycle                             ; while value in ECX not equal 0 we are repeat cycle
 
     pop ebp                               ; restoration stack
     ret 12                                ; exit of procedure
