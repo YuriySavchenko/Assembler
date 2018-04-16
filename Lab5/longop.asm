2fba9f14cefa8683c82bd7201edeb867bc498c02
YuriySavchenko <yurec.proger@gmail.com>
2018-04-17 02:41:50 +0300

    Update file longop.asm

--- Lab5/longop.asm
+++ Lab5/longop.asm
@@ -1,7 +1,9 @@
 section .data
     
+    ; variables which will be using as counters for loops
+
     counter dd 0
-    lengh dd 0
+    length dd 0
     Value_Ai dd 0
 
 section .code
@@ -9,8 +11,8 @@ section .code
 ; procedure for adding number increased bit rate
 
 Add_608_LONGOP:
-    push ebp
-    mov ebp, esp
+    push ebp                              ; write EBP to STACK
+    mov ebp, esp                          ; write pinter on STACK to register EBP
     
     mov esi, [ebp+16]                     ; ESI - adress of A
     mov ebx, [ebp+12]                     ; EBX - adress of B
@@ -33,22 +35,22 @@ Add_608_LONGOP:
     jnz .cycle                            ; while value in ECX not equal 0 we are repeat cycle
 
     pop ebp                               ; restoration stack
-    ret 12                                ; exit_proc of procedure
+    ret 12                                ;@exit_proc of procedure
 
 
 ; procedure for subtraction numbers increased bit rate
 
 Sub_512_LONGOP:
-    push ebp 
-    mov ebp, esp
+    push ebp                              ; write EBP to STACK
+    mov ebp, esp                          ; write pinter on STACK to register EBP
     mov esi, [ebp+16]                     ; ESI - adress of A
-    mov ebx, [ebp+12]                    ; EBX - adress of B
-    mov edi, [ebp+8]                     ; EDI - adress of Result
+    mov ebx, [ebp+12]                     ; EBX - adress of B
+    mov edi, [ebp+8]                      ; EDI - adress of Result
     
-    mov ecx, 16                          ; count of repeat
-    mov edx, 0                           ; number of 32-bit group 
+    mov ecx, 16                           ; count of repeat
+    mov edx, 0                            ; number of 32-bit group 
 
-    clc                                  ; set null bit CF for register EFLAGS
+    clc                                   ; set null bit CF for register EFLAGS
     
     .cycle:
     
@@ -62,16 +64,16 @@ Sub_512_LONGOP:
     jnz .cycle                            ; while value in ECX not equal 0 we are repeat cycle
 
     pop ebp                               ; restoration stack
-    ret 12                                ; exit_proc of procedure
+    ret 12                                ;@exit_proc of procedure
 
 ; procedure for multiplication number on 32-bit number
 
 Mul_Nx32_LONGOP:
-    push ebp
-    mov ebp, esp
+    push ebp                              ; write EBP to STACK
+    mov ebp, esp                          ; write pinter on STACK to register EBP
     mov ecx, [ebp+8]                      ; setup counter
-    mov esi, [ebp+16]                     ; ESI = address B
-    mov ebp, [ebp+12]                     ; EBP = address A
+    mov esi, [ebp+16]                     ; ESI = address A
+    mov ebp, [ebp+12]                     ; EBP = address B
     
     xor ebx, ebx                          ; set null value for EBP
     xor edi, edi                          ; set null value for EDI
@@ -93,67 +95,67 @@ Mul_Nx32_LONGOP:
     jnz .cycle                            ; while value in ECX not equal 0 we are repeat cycle
 
     pop ebp                               ; restoration stack
-    ret 12                                ; exit_proc of procedure
+    ret 12                                ;@exit_proc of procedure
 
 ; procedure for multiplication number on number
 
 Mul_NxN_LONGOP:
-	push ebp
-	mov ebp, esp
-	mov eax, [ebp+8]
-	mov [lengh], eax
-	mov esi, [ebp+12]
-	mov edi, [ebp+16]
-	mov ebp, [ebp+20]
+
+	push ebp                              ; write EBP to STACK
+	mov ebp, esp                          ; write pointer on STACK to register EBP
+	mov eax, [ebp+8]                      ; move value counter to EAX
+	mov [length], eax                     ; write value from EAX in variable length
+	mov esi, [ebp+12]                     ; ESI = address B 
+	mov edi, [ebp+16]                     ; EDI = address A
+	mov ebp, [ebp+20]                     ; EBP = address RESULT
 	
-	mov dword [counter], 0
+	mov dword [counter], 0                ; setup variable counter as null
 
-	@cycle_Mul_NxN_out:			; початок зовнішнього циклу1
+	@cycle_Mul_NxN_out:			          ; begin of first loop
 
-		mov eax, [counter]			
-		inc eax					; збільшуємо лічильник на одиницю
-		cmp eax, [lengh]
-		jg exit_proc					; вихід з циклу
-		mov [counter], eax
+	mov eax, [counter]			          ; write value from variable counter to EAX
+	inc eax				                  ; increment EAX	
+	cmp eax, [length]                     ; equal EAX and variable length 
+	jg @exit_proc					      ; exit of program
+	mov [counter], eax                    ; write value from EAX to variable counter
 
+	mov ecx, [length]			          ; setup counter for second loop
+	mov ebx, dword [esi+4*eax-4]	      ; write to memory Аix32
+	mov [Value_Ai], ebx                   ; write value from EBX to variable Value_Ai
 
-		mov ecx, [lengh]			; встановлення лічильника внутрішнього циклу
+	xor ebx, ebx				          ; set null value for EBX
 
-		mov ebx, dword [esi+4*eax-4]	; записуємо в пам'ять множник Аix32
-		mov [Value_Ai], ebx
+	@cycle_Mul_NxN_in:                    ; begin of second loop
 
-
-		xor ebx, ebx				; обнуляємо ebx		
-
-		@cycle_Mul_NxN_in:
-			mov eax, Value_Ai
-			mul dword [edi+4*ebx]
-			
-									; встановлення переносу в нуль
-			add dword [ebp+4*ebx], eax
-			adc dword [ebp+4*ebx+4], edx
-			
-			jnc @not_res_cor
-
-				mov eax, 1
-				add eax, ebx
-				stc ; встановлення переносу в одиницю
-
-				res_cor2:
-					inc eax
-					 
-					adc dword [ebp+4*eax], 0
-					jb res_cor2
-
-			@not_res_cor:
-
-			inc ebx					; збільшення зсуву на 1
-			dec ecx					; зменшуємо лічильник на 1
-			jnz @cycle_Mul_NxN_in	; якщо лічильник не 1, то перехід на мітку cycle_Mul_NxN
+	mov eax, [Value_Ai]                   ; write value from Value_Ai to EAX
+	mul dword [edi+4*ebx]                 ; multiplication of operands
 		
-		add ebp,4					; збільшення запису результату зсуву на 1
-		jmp @cycle_Mul_NxN_out
+    clc								      ; set null bit CF for register EFLAGS
+	add dword [ebp+4*ebx], eax            ; addition without transfer
+	adc dword [ebp+4*ebx+4], edx          ; addition with transfer
+	
+	jnc @not_res_cor                      ; repeat loop
 
-		exit_proc:
-	        pop ebp
-	        ret 16
+	mov eax, 1                            ; write digit 1 in EAX
+	add eax, ebx                          ; addition EAX and EBX without transfer
+	stc                                   ; setup transfer as digit 1  
+
+	res_cor2:                             ; begin of res_cor2
+	inc eax                               ; increment EAX
+		 
+	adc dword [ebp+4*eax], 0              ; add value from address and transfer
+	jb res_cor2                           ; go to begin res_cor2
+
+	@not_res_cor:                         ; begin of loop not_res_cor
+
+	inc ebx					              ; increment EBX 
+	dec ecx				                  ; decrement ECX
+	jnz @cycle_Mul_NxN_in	              ; if counter not equal 1 then go to cycle_Mul_NxN
+		
+	add ebp,4					          ; add digit 4 to register EBP
+	jmp @cycle_Mul_NxN_out                ; repeat cycle_Mul_NxN_out
+
+@exit_proc:                               ; procedure for exit of program
+
+    pop ebp                               ; delete pointer on STACK
+    ret 16                                ; stop procedure
