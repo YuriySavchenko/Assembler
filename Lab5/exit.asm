f72b4e4368d30dbb4fbac8a912bd9f0b9d0812cd
YuriySavchenko <yurec.proger@gmail.com>
2018-04-22 16:42:16 +0300

    Remove trash

--- Lab5/exit.asm
+++
@@ -1,9 +0,0 @@
-section .code
-    
-exit:
-    mov eax, 1      ; sys_exit
-    mov ebx, 0      ; code of error
-    
-    int 0x80        ; stop processor 
-
-    ret             ; exit of procedure
