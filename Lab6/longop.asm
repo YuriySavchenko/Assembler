dc9a7a869937da42a8072d9e4d6cc543c62164ef
YuriySavchenko <yurec.proger@gmail.com>
2018-05-13 23:01:17 +0300

    Uploaded Laboratory6

---
+++ Lab6/longop.asm
@@ -0,0 +1,52 @@
+segment readable executable
+
+SHR_LONGOP_PROC:
+    push ebp
+    mov ebp, esp
+    
+    mov eax, [ebp+8]                        ; number for stilling
+    mov edx, [ebp+12]                       ; value for shift in right
+
+    xor edi, edi
+    xor ebx, ebx
+    xor ecx, ecx
+
+    ;mov ecx, 24
+    mov ecx, 1
+
+    shr byte [edx+ecx*4], 1
+    adc edi, 0
+    clc
+
+    shr byte [eax+4*ebx], 1
+
+    jnc .label0
+    or dword [edx+ecx*4], 080000000h
+    jmp .label1
+
+    .label0:
+    and dword [edx+ecx*4], 07FFFFFFFh
+
+    .label1:
+    dec ecx 
+    
+    ;.loop:
+    ;cmp edi, 0
+    ;jz .label2
+    ;or dword [edx+ecx*4], 080000000h
+    ;jmp .label3
+    
+    ;.label2:
+    ;and dword [edx+ecx*4], 07FFFFFFFh 
+
+    ;.label3:
+    ;shr byte [edx+ecx*4], 1
+    ;adc edi, 0
+    ;clc
+    ;dec ecx 
+    ;jnz .loop
+     
+    pop ebp
+
+    ret 8
+
