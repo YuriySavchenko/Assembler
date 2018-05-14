9b083cd19c40dcfaef9757646f83c7f583df3f91
YuriySavchenko <yurec.proger@gmail.com>
2018-05-14 14:41:48 +0300

    Update longop.asm

--- Lab6/longop.asm
+++ Lab6/longop.asm
@@ -10,13 +10,10 @@ SHR_LONGOP_PROC:
     xor edi, edi
     xor ebx, ebx
     xor ecx, ecx
-    xor esi, esi
 
     mov ecx, 23
 
-    mov esi, [edx+ecx*4]
     shr dword [edx+ecx*4], 1
-    mov esi, [edx+ecx*4]
     adc edi, 0
     clc
 
@@ -24,12 +21,10 @@ SHR_LONGOP_PROC:
 
     jnc .label0
     or dword [edx+ecx*4], 080000000h
-    mov esi, [edx+ecx*4]
     jmp .loop
 
     .label0:
     and dword [edx+ecx*4], 07FFFFFFFh
-    mov esi, [edx+ecx*4]
 
     .loop:
     dec ecx
